0df1daf6e4f9a08002ba2f642cd49ee55179fe7a
SashaKhyzhun <sasha6427@gmail.com>
2015-07-14 20:02:39 +0300

    2

---
+++ Lab2.AirTickets/lab2String/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace lab2String
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelT1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показати рейси та квитки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 25);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(356, 228);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(14, 282);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(635, 228);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(374, 76);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Показати пункти призначення";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(665, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = " ";
+            // 
+            // labelT1
+            // 
+            this.labelT1.AutoSize = true;
+            this.labelT1.Location = new System.Drawing.Point(665, 26);
+            this.labelT1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelT1.Name = "labelT1";
+            this.labelT1.Size = new System.Drawing.Size(10, 13);
+            this.labelT1.TabIndex = 5;
+            this.labelT1.Text = " ";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(374, 127);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 47);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Вільні місця";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(665, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Рейси";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 267);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Квитки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 521);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.labelT1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "AirTickets";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelT1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab2.AirTickets/lab2String/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace lab2String
+{
+    public partial class Form1 : Form
+    {
+        int lines1 = System.IO.File.ReadAllLines("E:/Education/2 курс/Програмування/C#/labka2/Авіарейси.txt").Length;
+        int lines2 = System.IO.File.ReadAllLines("E:/Education/2 курс/Програмування/C#/labka2/Квитки.txt").Length;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Font = new Font("Arial", 11);
+            label1.Font = new Font("Arial", 14, FontStyle.Italic);
+            label2.Font = new Font("Arial", 14, FontStyle.Italic);
+            label2.ForeColor = System.Drawing.Color.Green;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView2.RowHeadersVisible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            string[] str;
+
+
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].HeaderCell.Value = "Номер рейсу";
+            dataGridView1.Columns[1].HeaderCell.Value = "Пункт відправлення";
+            dataGridView1.Columns[2].HeaderCell.Value = "Час вильоту";
+            dataGridView1.Columns[3].HeaderCell.Value = "Загальна к-сть місць";
+
+           
+            dataGridView1.Columns[1].Width = 150;
+            dataGridView1.Columns[3].Width = 120;
+          
+
+            dataGridView2.ColumnCount = 8;
+            dataGridView2.Columns[0].HeaderCell.Value = "Номер квитка";
+            dataGridView2.Columns[1].HeaderCell.Value = "Номер рейсу";
+            dataGridView2.Columns[2].HeaderCell.Value = "Місце";
+            dataGridView2.Columns[3].HeaderCell.Value = "Дата вильоту";
+            dataGridView2.Columns[4].HeaderCell.Value = "Пункт призначення";
+            dataGridView2.Columns[5].HeaderCell.Value = "Дата прибуття";
+            dataGridView2.Columns[6].HeaderCell.Value = "Час прибуття";
+            dataGridView2.Columns[7].HeaderCell.Value = "Ціна";
+
+            using (System.IO.StreamReader read = new System.IO.StreamReader("E:/Education/2 курс/Програмування/C#/labka2/Авіарейси.txt"))
+            {
+
+
+                str = read.ReadToEnd().Split(new Char[] { ' ', '\r' });
+                int pos = 0;
+                for (int i = 0; i < lines1; i++)
+                {
+                    dataGridView1.Rows.Add();
+                    for (int j = 0; j < 4; j++)
+                    {
+
+                        if (str[pos] != "")
+
+                            dataGridView1[j, i].Value = str[pos];
+                        pos++;
+                    }
+
+                }
+            }
+
+
+            using (System.IO.StreamReader read = new System.IO.StreamReader("E:/Education/2 курс/Програмування/C#/labka2/Квитки.txt"))
+            {
+
+
+                str = read.ReadToEnd().Split(new Char[] { ' ', '\r' });
+                int pos = 0;
+                for (int i = 0; i < lines2; i++)
+                {
+                    dataGridView2.Rows.Add();
+                    for (int j = 0; j < 8; j++)
+                    {
+
+                        if (str[pos] != "")
+
+                            dataGridView2[j, i].Value = str[pos];
+                        pos++;
+                    }
+
+                }
+            }
+
+
+
+
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            string[] timeStr;
+            int number = 0;
+            int ticket = 0;
+            int allTicket = 0;
+            string cities = "";
+
+            cities = "Пункти призначення, у які не продано жодного квитка" + "\n" + "на рейси, які відлітають з Києва після обіду.";
+            labelT1.Text = (cities);
+            cities = "";
+            for (int i = 0; i < lines1; i++)
+            {
+                ticket = 0;
+                allTicket = 0;
+
+                if (dataGridView1[1, i].Value.Equals("Kyiv"))
+                {
+                    
+                    timeStr = dataGridView1.Rows[i].Cells[2].Value.ToString().Split(new Char[] { ':' }); ;
+                    allTicket = Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+                    //MessageBox.Show(timeStr[0]);
+                    if (Convert.ToInt32(timeStr[0]) >= 14)
+                    {
+                        number = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+
+                        for (int j = 0; j < lines2; j++)
+                        {
+
+                            if (Convert.ToInt32(dataGridView2[1, j].Value) == number)
+                            {
+                                ticket++;
+                            }
+
+                        }
+
+
+                    }
+
+                    if (ticket == allTicket)
+                    {
+                        for (int j = 0; j < lines2; j++)
+                        {
+                            if (Convert.ToInt32(dataGridView2[1, j].Value) == number) { cities = cities + dataGridView2.Rows[j].Cells[4].Value.ToString() + "\n"; break; }
+
+
+                        }
+                    }
+
+
+                }
+            }
+            label1.Text = (cities);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> flightCity = new Dictionary<int, string>();
+            int[] flight = new int[lines1];
+            int[] numberOfPlaces = new int[lines1];
+            List<int>  numberOfAllPlaces = new List<int>();
+            string timeStr = "";
+
+            for (int i = 0; i < lines1; i++)
+            {
+                flightCity.Add(Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value), dataGridView1.Rows[i].Cells[1].Value.ToString());
+                numberOfAllPlaces.Add(Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value));
+                flight[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+             
+                for (int j = 0; j < lines2; j++)
+                {
+                    if (Convert.ToInt32(dataGridView2.Rows[j].Cells[1].Value) == flight[i]) { numberOfPlaces[i] = numberOfPlaces[i] + 1; } 
+                }
+            }
+
+            for (int i = 0; i < lines1; i++)
+            {
+                timeStr = timeStr + "На рейс " + flight[i] + " " + flightCity[flight[i]] + " кількість вільних місць:" + numberOfPlaces[i] + "/" + numberOfAllPlaces[i] + "\n"; 
+            }
+            label2.Text = (timeStr);
+        }
+    }
+}
